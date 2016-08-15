f68d2430ab83560b64b016fd0f43e238f7431359
GodofKim <kjh.original@gmail.com>
2016-08-15 13:49:18 +0900

    When signUp done=>go to profile-edit
    바로 프로필 수정하러 가도록 만듦.

--- Client/src/actions/index.js
+++ Client/src/actions/index.js
@@ -51,7 +51,7 @@ export function signupUser({ email, password}){
     .then(response => {
       dispatch({type:AUTH_USER});
       localStorage.setItem('token', response.data.token);
-      browserHistory.push('/feature');
+      browserHistory.push('/profile-edit');
     })
     .catch(error => {
       dispatch(authError(error.response.data.error));
