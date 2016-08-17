e4fcce556412a53835407b5c8455c98cb8635f5e
GodofKim <kjh.original@gmail.com>
2016-08-17 23:41:42 +0900

    Add Show Button at LetterSlot

--- Client/src/components/letterSlot.js
+++ Client/src/components/letterSlot.js
@@ -5,13 +5,16 @@ class LetterSlot extends Component {
     var letter = this.props.letter;
 
     return(
-      <li className="list-group-item">
-        <h3>{letter.nickname}</h3>
-        <h5>{letter.name}</h5>
-        <p>{letter.content}</p>
-        <p>{new Date(letter.date).getFullYear()}-{new Date(letter.date).getMonth()}
-          -{new Date(letter.date).getDate()}</p>
-      </li>
+      <div>
+        <li className="list-group-item">
+          <h3>{letter.nickname}</h3>
+          <h5>{letter.name}</h5>
+          <p>{letter.content}</p>
+          <p>{new Date(letter.date).getFullYear()}-{new Date(letter.date).getMonth()}
+            -{new Date(letter.date).getDate()}</p>
+        </li>
+        <button className="btn btn-primary">내용 보기</button>
+      </div>
     );
   }
 }
