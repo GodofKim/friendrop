211de5aabc714262d8c8aa3390235d4be5bc2e01
GodofKim <kjh.original@gmail.com>
2016-08-29 19:43:40 +0900

    Modify 'contactSlot' to hide the contact in default
    버튼누르면 땋 땋

--- Client/src/components/contactSlot.js
+++ Client/src/components/contactSlot.js
@@ -5,6 +5,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class ContactSlot extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isHidden : true
+    };
+
+    this.toggleContentHide = this.toggleContentHide.bind(this);
+  }
+
+  toggleContentHide (){
+    this.setState({
+      isHidden: !this.state.isHidden
+    });
+  }
+
     render() {
       var contact = this.props.contact;
       var profilePath = '/profile/' + contact.email;
@@ -13,9 +29,10 @@ class ContactSlot extends Component {
         <li className="list-group-item">
           <h3>{contact.nickname}</h3>
           <h5>{contact.name}</h5>
-          <p>{contact.phone}</p>
+          <p>{this.state.isHidden? null : contact.phone}</p>
           <p>{new Date(contact.date).getFullYear()}-{new Date(contact.date).getMonth()}
               -{new Date(contact.date).getDate()}</p>
+          <button className="btn btn-primary" onClick={this.toggleContentHide}>연락처 보기</button>
           <Link className="btn btn-primary" to={profilePath}>프로필 보기</Link>
          </li>
       );
